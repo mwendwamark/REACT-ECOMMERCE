3a0ddfeeab3611c78456d6945a1120dc690858d2
mwendwamark <mwendwamark310@gmail.com>
2024-10-31 18:26:33 +0300

    create pages and routing logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Card from "./pages/Card";
+import Login from "./pages/Login";
+import PlaceOrder from "./pages/PlaceOrder";
+import Orders from "./pages/Orders";
 
 const App = () => {
   return (
-    <div>
-      
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] 1g:px-[9vw]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Card />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
